09fcd536fa6fb9b268ae14179ac2cbf76d9f657e
Fahim1556 <49098556+Fahim1556@users.noreply.github.com>
2020-10-27 19:20:10 +0600

    second commit

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyCBwbIny-UkQXIE-PtZQY-TmJ0dV1nkhl4",
-  authDomain: "ema-john-simple-c0f63.firebaseapp.com",
-  databaseURL: "https://ema-john-simple-c0f63.firebaseio.com",
-  projectId: "ema-john-simple-c0f63",
-  storageBucket: "ema-john-simple-c0f63.appspot.com",
-  messagingSenderId: "875753041701",
-  appId: "1:875753041701:web:1edc94bffd15b4ae0fb4cc"
+  apiKey: "AIzaSyCMgIc7I-1zaNf_vihhLYmQi34JHD7IliE",
+  authDomain: "ema-john-payment-getway.firebaseapp.com",
+  databaseURL: "https://ema-john-payment-getway.firebaseio.com",
+  projectId: "ema-john-payment-getway",
+  storageBucket: "ema-john-payment-getway.appspot.com",
+  messagingSenderId: "591535815965",
+  appId: "1:591535815965:web:dbc5903b13f7727a8bb5f4"
 };
 
 export default firebaseConfig ;
